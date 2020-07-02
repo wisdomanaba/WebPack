4cc3e9d9556f18640d3eee65ec39502fa69f7e0d
Wisdom Anaba <50532579+wisdomanaba@users.noreply.github.com>
2020-07-02 01:01:01 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Switch, Route } from 'react-router'
 import Home from './pages/homePageComponent'
+import About from './pages/aboutPageComponent'
 
 export default function app() {
     return (
         <Switch>
             <Route path="/" render={props => (
                 <Home {...props} />
+            )}/>
+            <Route path="/about" render={props => (
+                <About {...props} />
             )}/>
         </Switch>
     )
